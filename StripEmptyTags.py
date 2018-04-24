a379b37974169a45af48d028d8995451ad1eb7a9
mac <mac@MacBook-Pro.local>
2018-04-24 17:56:38 +0300

    Python's BeautifulSoup is painfully slow even on small documents (less than 15M); use Jsoup instead.

--- StripEmptyTags.py
+++ StripEmptyTags.py
@@ -5,9 +5,34 @@ if python_implementation() == 'Jython':
     import import_site_packages
     import sqlite3
 from io import open as iopen
-from bs4 import BeautifulSoup 
+from java.io import File
+from sys.path import append as spappend
+spappend("./target/minimal-pom-1.0-SNAPSHOT-jar-with-dependencies.jar")
+from org.jsoup import Jsoup
+from org.jsoup.nodes import Element
+from org.jsoup.select import NodeTraversor, NodeVisitor
 from sys import argv
 #from nltk.book import *
+
+class MyVisitor(NodeVisitor):
+    def __init__(self):
+        super(NodeVisitor, self).__init__()
+        self._strip_list = []
+
+    def head(self, node, depth):
+        if isinstance(node, Element):
+            if len(node.ownText()) == 0 \
+                 and len(node.children()) == 0 \
+                 and node.tag().toString() not in [ "br" ]:
+                self._strip_list.append(node)
+
+    def tail(self, node, depth):
+        pass
+
+    def strip_empty_tags(self):
+        for e in self._strip_list:
+            e.remove()
+  
 
 if len(argv) < 3:
 	infile = "/Users/mac/Downloads/im_01"
@@ -17,9 +42,10 @@ else:
 	outfile = argv[2]
 
 with iopen(outfile, "w", encoding="utf-8", errors="ignore") as output:
-    soup = BeautifulSoup(iopen(infile, "r", encoding="utf-8", errors="ignore"), "html.parser")
-    for element in soup.find_all(): 
-        if len(element.text) == 0:
-            element.extract()
-    output.write(soup)
-    output.close()
+    input = File(infile)
+    soup = Jsoup.parse(input, "UTF-8", "")
+    visitor = MyVisitor()
+    NodeTraversor(visitor).traverse(soup);
+    visitor.strip_empty_tags()
+
+    output.write(soup.outerHtml())
---
+++ StripEmptyTags.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+JYTHON_HOME=/opt/local/share/java/jython
+HEAP_SZ=6
+
+if [ -n ${1} ]; then
+	CMD="StripEmptyTags.py a b"
+fi
+
+TERM=xterm-color \
+	/usr/bin/java \
+	-Xss2560k \
+	-Xnoclassgc \
+	-Xms${HEAP_SZ}G \
+	-Xmn$(( ${HEAP_SZ} / 2 ))G \
+	-classpath ${JYTHON_HOME}/jython.jar:./target/minimal-pom-1.0-SNAPSHOT-jar-with-dependencies.jar:. \
+	-Dpython.home=${JYTHON_HOME} \
+	-Dpython.executable=${JYTHON_HOME}/bin/jython \
+	-Dpython.launcher.uname=darwin \
+	-Dpython.launcher.tty=true \
+	-Dpython.cachedir=/Users/mac/.jython_cachedir \
+	-Dfile.encoding=UTF-8 \
+	org.python.util.jython \
+	${CMD}
+
